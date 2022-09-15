411cea4c6f96810a84b15fbe9333c4567f08ea7f
Kacper PC <kukulakacper@outlook.com2>
2022-09-15 19:20:51 +0200

    version 1.0

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Macierze_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////////  1 test
+            var arr1 = new int[3, 3]
+            {
+                {1, 2, 3},
+                {1, 2, 3},
+                {1, 2, 3}
+            };
+            Console.WriteLine(DiagonalAbsolue(3, arr1));
+
+            //////////////////////////////////////////  2 test
+            var arr2 = new int[3, 3]
+            {
+                {10, 2, 5},
+                {1, 10, 3},
+                {5, 2, 10}
+            };
+            Console.WriteLine(DiagonalAbsolue(3, arr2));
+
+            //////////////////////////////////////////  3 test
+            var arr3 = new int[3, 3]
+            {
+                {10, 2, 5},
+                {1, 10, 3},
+                {5, 2, 10}
+            };
+            Console.WriteLine(DiagonalAbsolue(4, arr3));
+        }
+
+        static int DiagonalAbsolue(int n, int[,] arr) {
+            
+            int SumR = 0, SumL = 0;
+
+            try{
+                for(int i=0; i<n; i++) {
+                    SumL += arr[i, i];
+                    SumR += arr[Math.Abs(i-n+1), i];
+                }
+            }
+            catch(System.Exception){
+                return -1;
+            }
+
+            //Console.WriteLine($"SumR: {SumR}, SumL {SumL}");
+            return Math.Abs(SumR-SumL);
+        }
+    }
+}
